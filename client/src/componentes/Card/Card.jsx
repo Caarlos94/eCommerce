baa34477ae422e5c0e3c225f824d1cd2195c9127
belidavalos11 <belidavalos.bd@gmail.com>
2022-12-07 16:25:04 -0300

    base cards

--- client/src/componentes/Card/Card.jsx
+++ client/src/componentes/Card/Card.jsx
@@ -1,1 +1,13 @@
-hola
+import React from 'react';
+import style from './Card.module.css'
+
+export default function Card({ name, image, id, branch, price}){
+    return(
+        <div className={style.card}>
+            <img src={image} alt="Not found" className={style.img}/>
+            <span className={style.name}>{name.charAt(0).toUpperCase() + name.slice(1)}</span>
+            <span className={style.branch}>{branch.charAt(0).toUpperCase() + branch.slice(1)}</span>
+            <span className={style.price}></span>
+        </div>
+    )
+}
