2113c9024338400d5c88599ff9e05210f65e88bf
belidavalos11 <belidavalos.bd@gmail.com>
2022-12-08 09:28:58 -0300

    base cards y paginado

--- client/src/componentes/Card/Card.jsx
+++ client/src/componentes/Card/Card.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import style from './Card.module.css'
+import { getProducts } from '../../redux/actions/actions'
 
 export default function Card({ name, image, id, branch, price}){
     return(
         <div className={style.card}>
             <img src={image} alt="Not found" className={style.img}/>
-            <span className={style.name}>{name.charAt(0).toUpperCase() + name.slice(1)}</span>
+            <span className={style.name}>{getProducts.name.charAt(0).toUpperCase() + getProducts.name.slice(1)}</span>
             <span className={style.branch}>{branch.charAt(0).toUpperCase() + branch.slice(1)}</span>
             <span className={style.price}></span>
         </div>
