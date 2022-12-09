19fc1e2d2c75aae2d2834ad22e40444273bf9439
cesarzv <cesar-x26@hotmail.com>
2022-12-09 15:18:41 -0500

    cambios

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,4 @@
 import './App.css';
-// // <<<<<<< HEAD
-// import { Route } from 'react-router-dom';
-// import Home from './componentes/home/home.jsx';
-// import SearchXname from './componentes/searchXname/SearchXname.jsx';
-// import Details from './componentes/details/details.jsx';
-// // import {BrowserRouter} from 'react-router-dom';
-
-// function App() {
-//   return (
-//     <div>
-//       <Route exact path="/">
-//         <Home />
-//       </Route>
-//       <Route exact path="/details">
-//         <Details />
-//       </Route>
-//       <Route path="/filtrados/:id" component={SearchXname} />
-//     </div>
-// =======
-import Navbar from './componentes/navbar/navbar.jsx';
 import SearchXname from './componentes/searchXname/SearchXname.jsx';
 import Home from './componentes/home/home.jsx';
 import Details from './componentes/details/details.jsx';
@@ -35,8 +15,6 @@ function App() {
 
       <Route path="/filtrados/:id" component={SearchXname} />
     </div>
-
-    // >>>>>>> development
   );
 }
 
