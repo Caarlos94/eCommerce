96883745f9bd1d576d8eea718510c39e1b2ab586
cesarzv <cesar-x26@hotmail.com>
2022-12-11 10:19:07 -0500

    mas estilos

--- client/src/componentes/home/home.jsx
+++ client/src/componentes/home/home.jsx
@@ -3,65 +3,66 @@ import { useEffect } from 'react';
 import { getProducts } from '../../redux/actions/actions.js';
 import s from './home.module.css';
 import Navbar from '../navbar/navbar.jsx';
-import Loading from '../../componentes/Loading/Loading'
+import Loading from '../../componentes/Loading/Loading';
 import Cards from '../Card/Cards';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import messiNotFound from '../../img/messiNotFound.gif'
-
+import messiNotFound from '../../img/messiNotFound.gif';
 
 const Home = () => {
   const dispatch = useDispatch();
-  const allProds = useSelector((state) => state.products)
-  const error = useSelector((state) => state.error)
+  const allProds = useSelector((state) => state.products);
+  const error = useSelector((state) => state.error);
   useEffect(() => {
     dispatch(getProducts());
-    /* dispatch(getCategorys()) */;
+    /* dispatch(getCategorys()) */
   }, [dispatch]);
 
-  const handlerClick = (e) => {
-    e.preventDefault();
-    dispatch(getProducts())
-  }
+  // const handlerClick = (e) => {
+  //   e.preventDefault();
+  //   dispatch(getProducts());
+  // };
 
   return (
     <div>
       <Navbar />
-      <NavLink to='/'>
-        <button onClick={e => handlerClick(e)}>Volver a cargar todos los productos!</button>
-      </NavLink>
+      {/* <NavLink to="/">
+        <button onClick={(e) => handlerClick(e)}>Clear filter</button>
+      </NavLink> */}
       <div className={s.hero}>
         <div className={s.textoHero}>
           <h1>Supra Sports</h1>
-          <button>AAAAAA</button>{/* botón de "ver colección" */}
+          <button>Ver coleccion</button>
+          {/* botón de "ver colección" */}
         </div>
 
         <div className={s.imgHero}>
-          <div>
+          <div className={s.messi}>
             <div className={s.img1}></div>
             <div className={s.img11}></div>
           </div>
-          <div>
+          <div className={s.extras}>
             <div className={s.img2}></div>
             <div className={s.img3}></div>
           </div>
         </div>
       </div>
-      {allProds.length > 0
-        ?
+      {allProds.length > 0 ? (
         <div className="App">
           {error ? (
             <div>
               <h1>No se encontraron coincidencias!</h1>
-              <img src={messiNotFound} alt='img'></img>
+              <img src={messiNotFound} alt="img"></img>
             </div>
-          ) : (<Cards />)}
+          ) : (
+            <Cards />
+          )}
         </div>
-        :
+      ) : (
         <div>
           <Loading />
         </div>
-      }
+      )}
     </div>
   );
 };
--- client/src/componentes/navbar/navbar.jsx
+++ client/src/componentes/navbar/navbar.jsx
@@ -1,27 +1,42 @@
-import React from "react";
-import SearchBar from "./searchBar/searchBar.jsx";
-import Filtros from "./filtros/filtros.jsx";
+import React from 'react';
+import SearchBar from './searchBar/searchBar.jsx';
+import Filtros from './filtros/filtros.jsx';
 import style from './navbar.module.css';
-import { NavLink } from "react-router-dom";
-
+import { NavLink } from 'react-router-dom';
+import heart from '../../img/heart-regular.svg';
+import user from '../../img/user.svg';
+import shopping from '../../img/shopping.png';
 
 const Navbar = () => {
-
-
   return (
     <div className={style.div}>
-      <SearchBar />
-
-      <Filtros />
-      <div>
-        <NavLink to='/product'>
-          <button>Publicar un producto!</button>
-        </NavLink>
+      <div className={style.black}></div>
+      <div className={style.white}>
+        <div className={style.publicar}>
+          <NavLink to="/product">
+            <button>Publicar un producto!</button>
+          </NavLink>
+        </div>
+        <div className={style.filtros}>
+          <Filtros />
+        </div>
+        <div className={style.searchBar}>
+          <SearchBar />
+        </div>
+        <div className={style.btns}>
+          <div className={style.btn}>
+            <img src={user} alt=""></img>
+          </div>
+          <div className={style.btn}>
+            <img src={heart} alt=""></img>
+          </div>
+          <div className={style.btn}>
+            <img src={shopping} alt=""></img>
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default Navbar
-
-
+export default Navbar;
