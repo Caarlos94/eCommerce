c0937d30c1ccaf7a3d3203bb50f33038c5706ced
Caarlos94 <carlos94_cr7@hotmail.com>
2022-12-11 08:48:10 -0600

    validaciones del form corregidas

--- client/src/componentes/ProductCreate/ProductCreate.jsx
+++ client/src/componentes/ProductCreate/ProductCreate.jsx
@@ -7,28 +7,39 @@ import style from './ProductCreate.module.css'
 
 const validate = (input, prods) => {
     let errors = {};
-    if (!(/^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/).test(input.nombre)) {
-        errors.nombre = 'Este dato es incorrecto... Es obligatorio, no se permiten caracteres especiales o números.'
+    if(input.nombre){
+        if (!(/^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/).test(input.nombre)) {
+            errors.nombre = 'Este dato es incorrecto... Es obligatorio, no se permiten caracteres especiales o números.'
+        }
+        if (prods.some(e => e.nombre.toUpperCase() === input.nombre.toUpperCase())) {
+            errors.nombre = 'Este producto ya existe!'
+        }
     }
-    if (prods.some(e => e.nombre.toUpperCase() === input.nombre.toUpperCase())) {
-        errors.nombre = 'Este producto ya existe!'
+    if(input.URL){
+        if (!(/(https?:\/\/.*\.(?:png|jpg|jpeg))/i).test(input.URL)) {
+            errors.URL = 'Este dato es obligatorio, solo permite imágenes de tipo .jpg/.png/.jpeg'
+        }
     }
-    if (!(/(https?:\/\/.*\.(?:png|jpg|jpeg))/i).test(input.URL)) {
-        errors.URL = 'Este dato es obligatorio, solo permite imágenes de tipo .jpg/.png/.jpeg'
+    if(input.precio){
+        if (input.precio < 1) {
+            errors.precio = 'Este dato es obligatorio, solo permite números mayores a uno.'
+        }
     }
-    if (input.precio < 1) {
-        errors.precio = 'Este dato es obligatorio, solo permite números mayores a uno.'
+    if(input.color) {
+        if (!(/^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/).test(input.color)) {
+            errors.color = 'Este dato es obligatorio, no se permiten caracteres especiales, números o espacios.'
+        }
     }
-    if (!(/^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/).test(input.color)) {
-        errors.color = 'Este dato es obligatorio, no se permiten caracteres especiales, números o espacios.'
+    if(input.talla) {
+        if (!(/^[A-Za-z0-9\s]+$/).test(input.talla)) {
+            errors.talla = 'Este dato es obligatorio, no se permiten caracteres especiales.'
+        }
     }
-    if (!(/^[A-Za-z0-9\s]+$/).test(input.talla)) {
-        errors.talla = 'Este dato es obligatorio, no se permiten caracteres especiales.'
+    if(input.marca) {
+        if (!(/^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/).test(input.marca)) {
+            errors.marca = 'Este dato es obligatorio, no se permiten caracteres especiales o números.'
+        }
     }
-    if (!(/^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/).test(input.marca)) {
-        errors.marca = 'Este dato es obligatorio, no se permiten caracteres especiales o números.'
-    }
-
     return errors;
 }
 
@@ -104,47 +115,42 @@ export default function ProdCreate() {
                     <div className={style.inputs}>
                         <label>Nombre: </label>
                         <input type='text' name='nombre' value={input.nombre} onChange={(e) => handlerChange(e)} ></input>
-                        {errors.nombre && (<p className={style.errors}>{errors.nombre}</p>)}
+                        {errors.nombre && <p className={style.errors}>{errors.nombre}</p>}
                     </div>
 
                     <div>
                         <label>Imagen: </label>
                         <input type='text' value={input.URL} name='URL' onChange={(e) => handlerChange(e)}
-                            disabled={!input.nombre || errors.nombre}></input>
-
-                        {errors.URL && !errors.nombre && (<p className={style.errors}>{errors.URL}</p>)}
+                        ></input>
+                        {errors.URL && <p className={style.errors}>{errors.URL}</p>}
                     </div>
 
                     <div>
                         <label>Precio </label>
                         <input type='number' value={input.precio} name='precio' onChange={(e) => handlerChange(e)}
-                            disabled={!input.nombre || !input.URL || errors.URL}></input>
-
-                        {errors.precio && !errors.URL && (<p className={style.errors}>{errors.precio}</p>)}
+                        ></input>
+                        {errors.precio && <p className={style.errors}>{errors.precio}</p>}
                     </div>
 
                     <div>
                         <label>Color: </label>
                         <input type='text' value={input.color} name='color' onChange={(e) => handlerChange(e)}
-                            disabled={!input.nombre || !input.URL || !input.precio || errors.precio}></input>
-
-                        {errors.color && !errors.precio && (<p className={style.errors}>{errors.color}</p>)}
+                        ></input>
+                        {errors.color && <p className={style.errors}>{errors.color}</p>}
                     </div>
 
                     <div>
                         <label>Talla: </label>
                         <input type='text' value={input.talla} name='talla' onChange={(e) => handlerChange(e)}
-                            disabled={!input.nombre || !input.URL || !input.precio || !input.color || errors.color}></input>
-
-                        {errors.talla && !errors.color && (<p className={style.errors}>{errors.talla}</p>)}
+                        ></input>
+                        {errors.talla && <p className={style.errors}>{errors.talla}</p>}
                     </div>
 
                     <div>
                         <label>Marca: </label>
                         <input type='text' value={input.marca} name='marca' onChange={(e) => handlerChange(e)}
-                            disabled={!input.nombre || !input.URL || !input.precio || !input.color || !input.talla || errors.talla}></input>
-
-                        {errors.marca && !errors.talla && (<p className={style.errors}>{errors.marca}</p>)}
+                        ></input>
+                        {errors.marca && <p className={style.errors}>{errors.marca}</p>}
                     </div>
 
                     <label>Categoría: </label>
