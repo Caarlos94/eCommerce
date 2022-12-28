ab2c8833485300bbcbe067d352dbb70be67d4cda
mederocc <mederocc@gmail.com>
2022-12-28 20:00:48 -0300

    ruta post de compras

--- server/src/routes/compraRouter.js
+++ server/src/routes/compraRouter.js
@@ -1,8 +1,21 @@
 const { Router } = require("express");
 const compraRouter = Router();
-const { Producto, Cliente, Compra, Compra_Producto } = require("../db");
+const { Producto, Cliente, Compra } = require("../db");
 
-// {clienteId:"e3d40d73-f6e1-4314-aacf-25919892852d", productos:[{prodId:"7ebe1230-86e2-11ed-9b4c-174d19eaa266", cantidad:1 }, {prodId:"7ebe6050-86e2-11ed-9b4c-174d19eaa266", cantidad:1 } ]}
+// Ejemplo de req.body:
+// {
+//   "clienteId":"32671d87-37bf-4014-af3c-bb6bd59a8f8d",
+//   "productos":[
+//     {
+//       "prodId":"3d995950-86eb-11ed-b779-997a2f11ad2c",
+//       "cantidad":1
+//     },
+//     {
+//       "prodId":"3d99f590-86eb-11ed-b779-997a2f11ad2c",
+//       "cantidad":1
+//     }
+//   ]
+// }
 
 compraRouter.post("/", async (req, res) => {
   try {
@@ -14,10 +27,14 @@ compraRouter.post("/", async (req, res) => {
       },
     });
 
+    if (!cliente) {
+      throw new Error("El cliente no se encuentra en la base de datos");
+    }
+
     const nuevaCompra = await Compra.create({ raw: true });
 
     nuevaCompra.clienteId = clienteId;
-    await nuevaCompra.save();
+    nuevaCompra.save();
 
     productos.forEach(async (producto) => {
       const foundProduct = await Producto.findByPk(producto.prodId);
