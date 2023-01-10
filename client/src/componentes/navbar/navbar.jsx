6874173b364768e6d0e4c4b1a33d229eb1bf109a
mederocc <mederocc@gmail.com>
2023-01-09 23:01:16 -0300

    validacion de QAForm es funcional. mejorable

--- client/src/componentes/navbar/navbar.jsx
+++ client/src/componentes/navbar/navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = ({ setPages }) => {
     };
     checkForAdminRole();
     user && dispatch(getFavorites(email));
-  }, [isAuthenticated, getAccessTokenSilently, dispatch, email]);
+  }, [isAuthenticated, getAccessTokenSilently, dispatch, email, user]);
 
   const favoritos = useSelector((state) => state.favorites);
 
